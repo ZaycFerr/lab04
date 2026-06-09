71ef307baa53a63f32956051e2922c5c14ebe8f9
ZaycFerr <Aglaya.zayc@mail.ru>
2026-06-09 19:31:30 +0100

    fix: очищен main.cpp от маркеров конфликта

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
+
 int main() {
-<<<<<<< HEAD
-    std::cout << "Hello" << std::endl;
-=======
-    std::cout << "Hello Travis CI!" << std::endl;
->>>>>>> b670f7278216123ce7fe3e780a6b7bb45a180001
+    std::cout << "Hello, GitHub Actions!" << std::endl;
     return 0;
 }
